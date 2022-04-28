c8a1180f8c91a2f6bbdde56f01bc20cc6eed263d
藤井涼平 <>
2022-04-28 09:43:26 +0900

    マイページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :sample_articles
   resources :articles
+  resources :mypage, only: :show
   root :to => 'articles#index'
   devise_for :users
+  get '/mypage', to: 'mypage#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
