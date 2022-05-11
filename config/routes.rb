a7fe6ad81457ba51fa21b509aa9de693473b86e5
藤井涼平 <>
2022-05-11 10:23:40 +0900

    検索フォームの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   resources :articles
-  resources :mypage, only: :show
+  # resources :mypage, only: :show
   root :to => 'articles#index'
   devise_for :users
   get '/mypage', to: 'mypage#show'
+  get '/search_article', to: 'articles#search'
+  get '/search_myarticle', to: 'mypage#search'
+  # get '/search_myarticle', to: 'mypage#search'
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
