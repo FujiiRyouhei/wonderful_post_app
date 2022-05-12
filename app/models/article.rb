8e5c5f980fedeebe4c2e57303b52fe5c8ccbe58a
藤井涼平 <>
2022-05-12 13:55:59 +0900

    tagモデルと中間テーブルを作成

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,8 @@ class Article < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
   belongs_to :user
+  has_many :article_tags, dependent: :destroy
+  has_many :tags, through: :article_tags
 
   def self.search(title)
     where(["title like ? ", "%#{title}%"])
