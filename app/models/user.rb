cff1f86d72d06762d6aa1ecdced65375493efd75
藤井涼平 <>
2022-05-31 21:54:43 +0900

    記事とタグの紐付けての作成と表示を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
-  has_many :articles
+  has_many :articles, dependent: :destroy
 end
